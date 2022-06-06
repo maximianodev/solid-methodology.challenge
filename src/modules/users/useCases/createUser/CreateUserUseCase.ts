3811689dba5c0412aab7044e2c44b49252d764a8
Max <lucas97lopes@hotmail.com>
2022-06-06 19:31:13 -0300

    [feature/createUserUseCase]: validate email already exists

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,6 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
     return this.usersRepository.create({ name, email });
   }
 }
